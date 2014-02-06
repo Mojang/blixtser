d34a65c6cb3553ac41b65cdd2ed8e19b3ae3867c
Amir Moulavi <amir.moulavi@gmail.com>
2014-02-06 21:26:43 +0100

    More tests for ClassSchemaBuilder

--- core/src/test/java/com/mojang/blixtser/core/ClassSchemaBuilderTest.java
+++ core/src/test/java/com/mojang/blixtser/core/ClassSchemaBuilderTest.java
@@ -1,21 +1,91 @@
 package com.mojang.blixtser.core;
 
-import com.mojang.blixtser.core.ClassSchemaBuilder;
 import org.junit.Assert;
 import org.junit.Test;
-
-import java.util.HashSet;
 
 public class ClassSchemaBuilderTest {
 
     private ClassSchemaBuilder classSchemaBuilder = new ClassSchemaBuilder();
 
     @Test
-    public void test_with_super_class() {
-        ClassSchemaBuilder.ClassInfo classInfo = classSchemaBuilder.createClassInfo(SerializableClass.class, new HashSet<String>());
+    public void super_class() {
+        ClassSchemaBuilder.ClassInfo classInfo = classSchemaBuilder.createClassInfo(TestClass.class);
         Assert.assertEquals(7, classInfo.fieldInfos.length);
     }
 
+    @Test
+    public void mergable_fields() {
+        ClassSchemaBuilder.ClassInfo classInfo = classSchemaBuilder.createClassInfo(ClassWithMergeableFields1.class);
+        Assert.assertEquals(1, classInfo.fieldInfos.length);
+    }
 
+    @Test
+    public void mergable_fields_and_super_class() {
+        ClassSchemaBuilder.ClassInfo classInfo = classSchemaBuilder.createClassInfo(ClassWithMergeableFields2.class);
+        Assert.assertEquals(1, classInfo.fieldInfos.length);
+    }
+
+    @Test
+    public void mergable_fields_and_super_class_and_volatile() {
+        ClassSchemaBuilder.ClassInfo classInfo = classSchemaBuilder.createClassInfo(ClassWithMergeableFields3.class);
+        Assert.assertEquals(4, classInfo.fieldInfos.length);
+    }
+
+
+    /**
+     *
+     */
+    private static class ClassWithMergeableFields1 {
+        private int a, b, c, d, e;
+        private long f, g, h;
+        private double i, j, k, l, m, n, o;
+        private float p, q, r;
+        private byte s, t, u;
+        private short v, w;
+        private boolean x, y, z;
+    }
+
+    /**
+     *
+     */
+    private static class ClassWithMergeableFields2 extends ClassWithMergeableFields1 {
+        private int a, b, c, d, e;
+        private long f, g, h;
+        private double i, j, k, l, m, n, o;
+        private float p, q, r;
+        private byte s, t, u;
+        private short v, w;
+        private boolean x, y, z;
+    }
+
+    /**
+     *
+     */
+    private static class ClassWithMergeableFields3 extends ClassWithMergeableFields2 {
+        private volatile int v1, v2;
+        private String a;
+    }
+
+
+    /**
+     *
+     */
+    private static class TestSuperClass {
+        private int a1;
+        private Integer a2;
+    }
+
+    /**
+     *
+     */
+    private static class TestClass extends TestSuperClass {
+        private String b;
+        private double c1;
+        private Double c2;
+        private float d1;
+        private Float d2;
+        private short e1;
+        private Short e2;
+    }
 
 }
